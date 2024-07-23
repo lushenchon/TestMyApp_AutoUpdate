7072edb7c2f7e212deea916b1c423e3351e121ce
LSC <lushenchon@sina.com>
2024-07-23 09:59:18 +0800

    7/23第三次

--- TestMyApp_AutoUpdate/Form1.cs
+++ TestMyApp_AutoUpdate/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoUpdaterDotNET;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,8 +21,19 @@ namespace TestMyApp_AutoUpdate
         private void Form1_Load(object sender, EventArgs e)
         {
             // 初始化自动更新
-            AutoUpdaterExample autoUpdater = new AutoUpdaterExample();
-            autoUpdater.ConfigureAutoUpdater();
+            //AutoUpdaterExample autoUpdater = new AutoUpdaterExample();
+            //autoUpdater.ConfigureAutoUpdater();
+            AutoUpdater.InstalledVersion = new Version("1.2");
+            System.Timers.Timer timer = new System.Timers.Timer
+            {
+                Interval = 1 * 30 * 1000,
+                SynchronizingObject = this
+            };
+            timer.Elapsed += delegate
+            {
+                AutoUpdater.Start("https://raw.githubusercontent.com/lushenchon/TestMyApp_AutoUpdate/master/update.xml");
+            };
+            timer.Start();
         }
 
 
