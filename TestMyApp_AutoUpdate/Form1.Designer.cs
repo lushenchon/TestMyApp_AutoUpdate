dd004b9da48efcdfec61d8b15aa0211cc3368c23
LSC <lushenchon@sina.com>
2024-07-23 10:25:39 +0800

    7/23第五次

--- TestMyApp_AutoUpdate/Form1.Designer.cs
+++ TestMyApp_AutoUpdate/Form1.Designer.cs
@@ -36,9 +36,9 @@ namespace TestMyApp_AutoUpdate
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(262, 126);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "检查更新";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -49,7 +49,7 @@ namespace TestMyApp_AutoUpdate
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "ui ";
+            this.Text = "v1.0.3";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
